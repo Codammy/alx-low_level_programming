d266e9fe3f2b863dbbe13873bee3980f635ffb80
Codammy <damisco005@gmail.com>
2022-07-01 05:43:23 -0700

    task 0

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 
-	int main() 
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
 {
+
 		printf("Holberton School");
-		return (0);
+
+			return (0);
+
 }
