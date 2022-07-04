81169a1d4162ab5ab7242411a27f52ca806e7dce
Codammy <damisco005@gmail.com>
2022-07-04 09:28:17 -0700

    task 0

--- 0x00-hello_world/0-main.c
+++ 0x00-hello_world/0-main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-	/**
-	 * added a headertfile
-	 * set main argument to void
-	 * return 0 added
-	 */ 
-	int main(void) 
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success
+ */
+int main(void)
 {
 	printf("Holberton School");
 	return (0);
