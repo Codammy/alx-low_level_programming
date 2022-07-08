2687fd2a29845c6e29516c7e0754fdde5f93d581
Codammy <damisco005@gmail.com>
2022-07-07 17:25:24 -0700

    compiled hard file

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int digit1, digit2, digit3;
 
-	for (digit 1 = 0; digit1 < 8; digit1++)
+	for (digit1 = 0; digit1 < 8; digit1++)
 	{
 		for(digit2 = digit1 +1; digit2 < 9; digit2++)
 		{
@@ -20,7 +20,7 @@ int main(void)
 				putchar((digit2 % 10) +'0');
 				putchar((digit3 % 10) + '0');
 
-				if(digit == 7 && digit2 == 8 && digit3 == 9)
+				if(digit1 == 7 && digit2 == 8 && digit3 == 9)
 				continue;
 
 				putchar(',');
