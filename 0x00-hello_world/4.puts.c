e00b99146d45ccc81b9cd4df061eacf6f75fd4ae
Codammy <damisco005@gmail.com>
2022-12-08 12:41:15 -0800

    first c file

---
+++ 0x00-hello_world/4.puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *printing to the standard output
+ *using puts
+ *return value(0)
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle\n");
+return (0);
+}
