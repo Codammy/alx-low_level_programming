bbd63158cfcfc6b528e03739c1991effd6c935d4
Codammy <damisco005@gmail.com>
2022-12-09 12:41:39 -0800

    revoking

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * printing the mode of a random int to stdo
+ * Return: (0)
+ */
+int main(void)
+{	
+	int alphb = 97;
+
+	while (alphb <= 122)
+	{
+		putchar(alphb);
+		alphb++;
+	}
+	return (0);
+}
