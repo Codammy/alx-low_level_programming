ef6827f9ba3036b9e06ccfa9c08b9dca4003b100
Codammy <damisco005@gmail.com>
2022-12-13 04:53:28 -0800

    first error fixed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,11 +2,20 @@
 
 /**
  * main - putchar
- * 
+ *
  *Return: 0
  */
-int main()
+int main(void)
 {
-  _putchar("_putchar");
-  return(0);
+	int s = _putchar('_');
+	int p = _putchar('p');
+	int u = _putchar('u');
+	int t = _putchar('t');
+	int c = _putchar('c');
+	int h = _putchar('h');
+	int a = _putchar('a');
+	int r = _putchar('r');
+	int newline = _putchar('\n');
+
+	return (0);
 }
