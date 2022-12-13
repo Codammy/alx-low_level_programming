b1f4ebb3c62664ebe631181d51c9a948f167db44
Codammy <damisco005@gmail.com>
2022-12-13 07:50:11 -0800

    second one

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet - prints a-z
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (j = 1; j <= 10; j++)
+	{
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	}
+	_putchar('\n');
+}
