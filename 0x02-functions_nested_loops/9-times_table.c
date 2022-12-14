54795a7c0a8d2752f7d3ac9e604292531e909892
Codammy <damisco005@gmail.com>
2022-12-14 14:37:58 -0800

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,29 +7,47 @@
  */
 void times_table(void)
 {
-	int x, y;
+	int x, j;
 
-	for (x = 0; x <= 9; x++)
-	{
-	for (y = 0; y <= 9; y++)
-	{
-		int z = x * y;
+for (x = 0; x <= 9; x++)
+{
+for (j = 0; j < 10; j++)
+{
+int z = j * x;
 
-		if (z <= 9)
-		{
-		_putchar(z + '0');
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		}
-		else
-		{
-		_putchar((z / 10) + '0');
-		_putchar((z % 10) + '0');
-		_putchar(',');
-		_putchar(' ');
-		}
-	}
-	_putchar('\n');
-	}
+if (z < 10)
+{
+if ((x == 0) && (z == 0))
+{
+_putchar(z + '0');
+if (j < 9)
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+}
+if ((x != 0) && (z == 0))
+{
+_putchar(z + '0');
+_putchar(',');
+}
+if (z > 0)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar(z + '0');
+if (j < 9)
+_putchar(',');
+}
+}
+else
+{
+_putchar(' ');
+_putchar(z / 10 + '0');
+_putchar(z % 10 + '0');
+if (j < 9)
+_putchar(',');
+}
+}
+_putchar('\n');
+}
 }
