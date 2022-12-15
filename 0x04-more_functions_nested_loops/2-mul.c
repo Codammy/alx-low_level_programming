3e564be411cd5fc0e5e511e0a6b16521da59bec4
Codammy <damisco005@gmail.com>
2022-12-15 13:55:55 -0800

    more functions

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * mul - check if a c is a digit or not.
+ * @a: first value
+ * @b: second value
+ * Return: x.
+ */
+int mul(int a, int b)
+{
+	int x = a * b;
+
+	return x;
+}
