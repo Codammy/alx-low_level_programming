37c14263bfaa8b413edc1f37c5fb139eaf22f840
Codammy <damisco005@gmail.com>
2022-12-15 16:06:04 -0800

    more functions

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,18 +8,18 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 1; i <= 10; i++)
+	{
 	for (j = 0; j <= 14; j++)
 	{
-	{
 	if (j < 10)
-	_putchar(i + '0');
+	_putchar(j + '0');
 	else
 	{
 	_putchar(j / 10 + '0');
 	_putchar(j % 10 + '0');
 	}
 	}
+	 _putchar('\n');
 	}
-	_putchar('\n');
 }
