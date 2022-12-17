663c9ce38aee8b1c5f993ca5788d9257d682c3eb
Codammy <damisco005@gmail.com>
2022-12-17 00:57:02 -0800

    times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_times_table - print times table from value n
  * @n : param value
@@ -15,11 +14,10 @@ for (i = 0; i <= n; i++)
 for (j = 0; j <= n; j++)
 {
 if (j == 0)
-{
 _putchar(i * j + '0');
-}
 if ((i * j < 10) && (j != 0))
 {
+_putchar(' ');
 _putchar(' ');
 _putchar(' ');
 _putchar(i * j + '0');
@@ -27,11 +25,13 @@ _putchar(i * j + '0');
 if ((i * j > 9) && (i * j < 100))
 {
 _putchar(' ');
+_putchar(' ');
 _putchar(i * j / 10 + '0');
 _putchar(i * j % 10 + '0');
 }
 if (i * j > 99)
 {
+_putchar(' ');
 _putchar(' ');
 _putchar(i * j / 10 + '0');
 _putchar(i * j % 10 + '0');
