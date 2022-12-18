64d8392bb35d12d034e097398391b06aaaa86cdb
Codammy <damisco005@gmail.com>
2022-12-18 07:00:08 -0800

    more c functions

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,12 +8,13 @@
 int main(void)
 {
 	int x = 0;
+	int n;
 
-	for (int n = 1; n < 1024; n++)
+	for (n = 1; n < 1024; n++)
 	{
 	if (n % 3 == 0 || n % 5 == 0)
 		x += n;
 	}
 	printf("%d", x);
-
+	return (0);
 }
