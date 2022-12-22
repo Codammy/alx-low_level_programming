5b7bf9b2adaed49ed932683f44d2605d6a09dc03
Codammy <damisco005@gmail.com>
2022-12-22 08:23:25 -0800

    more pointers and array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * reverse_array - prints the reverse of an aray to stdo
+ * @a: points to array
+ * n: size of array
+ */
+void reverse_array(int *a, int n)
+{
+	n -= 1;
+	while (n >= 0)
+	{
+		printf("%d", a[n]);
+		if (n != 0)
+			printf(", ");
+		n--;
+	}
+	putchar('\n');
+}
