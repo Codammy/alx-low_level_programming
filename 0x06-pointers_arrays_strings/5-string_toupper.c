627ee651f2bf45a5796e003fa0d0b119534d5d39
Codammy <damisco005@gmail.com>
2022-12-24 07:08:33 -0800

    more pointers and array

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * string_toupper - works exactly like strupr function
+ * Return: string_toupper
+ * @conv
+ */
+
+void test();
+char *string_toupper(char *conv)
+{
+int i;
+int length = strlen(conv);
+
+for (i = 0; i < length; i++)
+{
+if (conv[i] >= 97)
+{
+_putchar(conv[i] - 32);
+}
+else
+_putchar(conv[i]);
+}
+_putchar('\n');
+return (0);
+}
