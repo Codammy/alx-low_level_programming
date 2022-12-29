3198ecda5367905b49d797851eff826abb2b5f96
Codammy <damisco005@gmail.com>
2022-12-29 05:42:48 -0800

    more pointers and array in c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,18 +8,26 @@
  */
 char *cap_string(char *capt)
 {
+{
 int slength = strlen(capt);
+/*char c[slength - 1];*/
 int i;
 
-for (i = 0; i < slength; i++)
+for (i = 0; i <= slength; i++)
 {
-if (capt[i] >= 97)
-	capt[i] = capt[i] - 32;
-if (capt[i] == ' ')
+if (capt[0] >= 97)
 {
-	i++;
 capt[i] = capt[i] - 32;
+}
+if ((capt[i] == ' ') /*|| (s[i] == ' ' && s[i] == '.') || (s[i] == ' ' && s[i] == '\n') || (s[i] == ' ' && s[i] == '\t') || (s[i] == '.' && s[i] == '\n')*/ || (capt[i] == '\n') || (capt[i] == '\t') || (capt[i] == ',') || (capt[i] == ';') || (capt[i] == '.') || (capt[i] == '!') || (capt[i] == '?') || (capt[i] == '"') || (capt[i] == '(') || (capt[i] == ')') || (capt[i] == '{') || (capt[i] == '}'))
+{
+i++;
+if (capt[i] >= 97)
+        capt[i] = capt[i] - 32;
+else
+        capt[i] = capt[i];
 }
 }
 return (capt);
+}
 }
