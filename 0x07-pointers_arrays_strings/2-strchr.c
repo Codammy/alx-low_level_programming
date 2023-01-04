1df04b28717302d9e42cb881f203433062e2da57
Codammy <damisco005@gmail.com>
2023-01-03 18:19:08 -0800

    more c pointers and array

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strchr - locates a character in a string.
+ *
+ * @s: memory to check from.
+ * @c: value to check for.
+ *
+ * Return: (c);
+ */
+char *_strchr(char *s, char c)
+{
+	char * ret = strchr(s, c);
+
+	return (ret);
+}
