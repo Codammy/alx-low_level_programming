db5b97c9515b42b74c14a5d348d041f6e6035b7d
Codammy <damisco005@gmail.com>
2023-01-06 07:52:45 -0800

    Hands on recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
-#include <string.h>
 
 /**
  * _puts_recursion - prints a string in reverse follwed by a new line.
  *
  * @s: constant string
  *
- * Return: no rueturn.
+ * Return: no return.
  */
 void _puts_recursion(char *s)
 {
-	int x = strlen(s);
-puts(s[x]);
-x--;
-_puts_recursion(s);
+puts(s);
 }
