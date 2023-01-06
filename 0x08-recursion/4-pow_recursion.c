5d4aef7aa6f82d3b8d86646c985308ef0f3a5bcf
Codammy <damisco005@gmail.com>
2023-01-05 16:31:45 -0800

    working with maths functions

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,14 @@
+#include <math.h>
+
+/**
+ * _sqrt_recursion - retuens the square root of a number;
+ *
+ * @n: argument/parameter.
+ * Return: sqroot.
+ */
+int _sqrt_recursion(int n)
+{
+	int sqroot = sqrt(n);
+
+	return (sqroot);
+}
