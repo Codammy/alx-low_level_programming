e1448840be834e757a4ad06aaebb4f20f86b6ef1
Codammy <damisco005@gmail.com>
2023-01-06 07:46:49 -0800

    Hands on recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * _print_rev_recursion - reverses a string
@@ -8,12 +7,10 @@
 
 void _print_rev_recursion(char *s)
 {
-	int len = strlen(s) - 1;
-	char *new;
-
-	strncpy(new, s, len - 1);
-	if (len < 0)
+	if (*s == '\0')
 		return;
-	_putchar(s[len]);
-	_print_rev_recursion(new);
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
 }
