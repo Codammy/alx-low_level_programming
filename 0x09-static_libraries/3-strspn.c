28c2db80f3f432980fa62f73c1d02b1840808d89
Codammy <damisco005@gmail.com>
2023-01-08 23:30:55 -0800

    My static library

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strspn - gets the length of a prefix substring.
+ *
+ * @s: pointer.
+ * @accept: pointer.
+ *
+ * Return: (n);
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int n = strspn(s, accept);
+
+	return (n);
+}
