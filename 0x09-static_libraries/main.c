f016d9c8266dda4e33c7758a175b98d23b7cffdd
Codammy <damisco005@gmail.com>
2023-01-09 04:37:57 -0800

    committed lib

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - printsto stdo.
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+    _puts("\"My goal was to be the best hacker\"\n\t- Kevin Mitnick");
+    return (0);
+}
