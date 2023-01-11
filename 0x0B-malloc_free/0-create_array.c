3fd45c474440fad99b3c6e1cdfe85abe8334632d
Codammy <damisco005@gmail.com>
2023-01-11 05:32:59 -0800

    c malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	ptr = malloc(sizeof(char) * size);
+	ptr = (char *)malloc(sizeof(char) * size);
 
 	for (len = 0; len < size; len++)
 	ptr[len] = c;
