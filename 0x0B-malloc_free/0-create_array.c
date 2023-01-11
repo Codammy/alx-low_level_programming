ca13d8f2a1af5da9ed5d391f134804490fe28a76
Codammy <damisco005@gmail.com>
2023-01-11 04:23:55 -0800

    c malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,6 +5,19 @@
  * create_array - creates an array of chars and i itializes it withachar
  *
  * @size: array size
- * c: char
- * Return: null
+ * @c: char
+ * Return: null or ptr.
  */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+
+	if (size == 0)
+		return (NULL);
+	ptr = (char *)malloc(size * sizeof(char));
+	ptr[0] = c;
+
+	if (ptr == NULL)
+		return (NULL);
+	return (ptr);
+}
