9ff7ba54243a1a1a5a749f72fb9fbbf351eee83c
Codammy <damisco005@gmail.com>
2023-01-14 15:59:53 -0800

    c malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,20 +20,11 @@ char *str_concat(char *s1, char *s2)
 	len1 = strlen(s1);
 	len2 = strlen(s2);
 	lenlen = len1 + len2;
-	if (len1 == 0)
-	{
-		len1++;
-		if (len2 == 0)
-		len1--;
-	}
-	if (len2 == 0)
-		lenlen++;
-	if (len2 != 0 && len1 == 0)
-		len1--;
 	j = 0;
 	ptr = (char *) malloc((lenlen + 1) * sizeof(char));
 	if (ptr == NULL)
 		return (NULL);
+
 	for (i = 0; i <= len1 - 1; i++)
 		ptr[i] = s1[i];
 	for (i = len1; i <= lenlen - 1; i++)
