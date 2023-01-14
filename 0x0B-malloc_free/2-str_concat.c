bd54e1a08ad795c60659973584e1ab061acce02d
Codammy <damisco005@gmail.com>
2023-01-14 14:55:21 -0800

    c malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,12 +13,16 @@ char *str_concat(char *s1, char *s2)
 	int i, j, len1, len2, lenlen;
 	char *ptr;
 	if (s1 == NULL)
-		s1[0] = '\0';
+		s1 = "";
 	if (s2 == NULL)
-		s2[0] = '\0';
+		s2 = "";
 	len1 = strlen(s1);
 	len2 = strlen(s2);
 	lenlen = len1 + len2;
+	if (len1 == 0)
+		len1++;
+	if (len2 == 0)
+		lenlen++;
 	j = 0;
 	ptr = (char *) malloc((lenlen + 1) * sizeof(char));
 	if (ptr == NULL)
@@ -30,6 +34,7 @@ char *str_concat(char *s1, char *s2)
 		ptr[i] = s2[j];
 		j++;
 	}
+	if (ptr[lenlen + 1] != '\0')
 	ptr[lenlen + 1] = '\0';
 
 	return (ptr);
