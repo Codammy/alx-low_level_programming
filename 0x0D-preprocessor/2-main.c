2e30e84c77ecca4fc641066385db26968c2bcf8d
Codammy <damisco005@gmail.com>
2023-01-16 11:11:47 -0800

    using __FILE__ predefined macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints file name using __FILE__ standard predefined macro
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
