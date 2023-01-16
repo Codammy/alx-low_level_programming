527059137322565725ac25fe45bfd966336bf6cc
Codammy <damisco005@gmail.com>
2023-01-16 12:02:13 -0800

    change abs macro

---
+++ 0x0D-preprocessor/3-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "3-function_like_macro.h"
+#include "3-function_like_macro.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int i;
+    int j;
+
+    i = ABS(-98);
+    j = ABS(98);
+    printf("%d, %d\n", i, j);
+    return (0);
+}
