df1f85725f128a274595bf83c40b213dfe975e34
Codammy <damisco005@gmail.com>
2023-01-17 14:08:37 -0800

    structure and typedef

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * new_dog - creates new dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Return: NULL if function fails
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newDog = malloc(sizeof(dog_t));
+	if (newDog == 0 )
+		return (0);
+	(*newDog).name = name;
+	(*newDog).age = age;
+	(*newDog).owner = owner;
+
+	return(newDog);
+
+}
