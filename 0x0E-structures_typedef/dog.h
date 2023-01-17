04abef6fa61dc23b5e2ca189595828beba588134
Codammy <damisco005@gmail.com>
2023-01-17 04:47:26 -0800

    structure and typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
+#ifndef _HEADER_
+#define _HEADER_
+/**
+ * struct dog - A dog's information
+ * @name: Dogs name
+ * @age: Dogs age
+ * @owner: Dogs owner
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
