868f4f4c7ba51e5ab38d6f4bb9f0fc8b5044feb7
Codammy <damisco005@gmail.com>
2023-01-17 06:36:49 -0800

    structure and typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,12 +9,12 @@ void print_dog(struct dog *d)
 	if (d == 0)
 		return;
 /*	(*d).name = "bingo";*/
-		if ((*d).name == 0)
+		if (d->name == 0)
 			printf("(nil)");
 /*	(*d).age = 2;*/
-		if ((*d).age == 0)
+		if (d->age == 0)
 			printf("(nil)");
 /*	(*d).owner = "Mr Dam";*/
-		if ((*d).owner == 0)
+		if (d->owner == 0)
 			printf("(nil)");
 }
