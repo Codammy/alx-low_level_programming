cff186cae58a132245b9551e327645d9beb0d0c0
Codammy <damisco005@gmail.com>
2023-01-17 08:33:05 -0800

    structure and typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,17 +10,17 @@ void print_dog(struct dog *d)
 		return;
 
 	if (d->name == 0)
-		printf("nil\n");
+		printf("(nil)\n");
 	else
 		printf("Name: %s\n", d->name);
 
 	if (d->age == 0)
-		printf("nil\n");
+		printf("(nil)\n");
 	else
 		printf("Age: %f\n", d->age);
 
 	if (d->age == 0)
-		printf("nil\n");
+		printf("(nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
 }
