d3d7522fe7a421ce00a9fb93296a9a0414a0eb13
Codammy <damisco005@gmail.com>
2023-01-19 03:34:30 -0800

    function pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,26 +4,28 @@
  * @size: is the number of elements in the array
  * @cmp: pointer to function that checks for comparison
  * @array: pointer to array
+ *
+ * Return: returns the index
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int count = 0;
-	int index, not_zero, found = 0;
+int count = 0;
+int index, not_zero;
 
-	if (size <= 0)
-		return (-1);
-	if (array != 0 && cmp != 0)
-	{
-		while (count < size)
-		{
-			index = array[count];
-			not_zero = cmp(index);
-			if (not_zero == 1)
-			found += not_zero;
-			count++;
-		}
-		if (found == 0)
-			return (-1);
-	}
-	return (found);
+if (size <= 0)
+return (-1);
+if (array != 0 && cmp != 0)
+{
+while (count < size)
+{
+index = array[count];
+not_zero = cmp(index);
+if (not_zero == 1)
+break;
+count++;
+}
+if (not_zero == 0)
+return (-1);
+}
+return (count);
 }
