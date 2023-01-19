1049f0110f141fcb71439e31e3e8075839f69d8d
Codammy <damisco005@gmail.com>
2023-01-19 01:08:24 -0800

    function pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (array != NULL || action != NULL)
+	if (array != NULL && action != NULL)
 	{
 	unsigned int count = 0;
 	int index;
