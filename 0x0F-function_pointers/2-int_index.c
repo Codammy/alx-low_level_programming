cd2cee43f72c0134fb4e1ebd1f0abf6083bec3f9
Codammy <damisco005@gmail.com>
2023-01-19 04:31:25 -0800

    function pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,19 +13,18 @@ int count = 0;
 int index, not_zero;
 
 if (size <= 0)
-return (-1);
-if (array != 0 && cmp != 0)
-{
+	return (-1);
+if (array == 0 && cmp == 0)
+	return (-1);
 while (count < size)
 {
-index = array[count];
-not_zero = cmp(index);
-if (not_zero == 1)
-break;
-count++;
+	index = array[count];
+	not_zero = cmp(index);
+	if (not_zero == 1)
+	break;
+	count++;
 }
 if (not_zero == 0)
-return (-1);
-}
+	return (-1);
 return (count);
 }
