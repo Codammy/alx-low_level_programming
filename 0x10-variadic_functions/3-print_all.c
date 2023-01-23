a9dc1e2548fe1dfc3331449cbbe355b220435596
Codammy <damisco005@gmail.com>
2023-01-23 08:30:57 -0800

    Variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -5,48 +5,29 @@
 /**
  * print_all - A function that prints anything
  *
- * format: List of types of arguments passed to the function.
+ * @format: List of types of arguments passed to the function.
  * Return: Nothing.
  */
 void print_all(const char *const format, ...)
 {
-	int i = 0, len;
-	char c;
-	int in;
-	float f;
-	char *s;
+	int i = 0;
+	int len = strlen(format);
 
 	va_list p;
-	len = strlen(format);
 
 	va_start(p, format);
 
 	while (i < len)
 	{
-		if (format[i] != 's' && format[i] != 'c' && format[i] != 'i' && format[i] != 'f')
-		{i++;
-			continue;
-		}
 		if (format[i] == 'f')
-		{
-			f = va_arg(p, double);
-			printf("%f", f);
-		}
+			printf("%f", va_arg(p, double));
 		else if (format[i] == 's')
-		{
-			s = va_arg(p, char *);
-			printf("%s", s);
-		}
+			printf("%s", va_arg(p, char *));
 		else if (format[i] == 'i')
-		{
-			in = va_arg(p, int);
-			printf("%d", in);
-		}
+			printf("%d", va_arg(p, int));
 		else if (format[i] == 'c')
-		{
-			c = va_arg(p, int);
-			printf("%c", c);
-		}
+			printf("%c", va_arg(p, int));
+
 		if (i < len - 1)
 			printf(", ");
 		i++;
