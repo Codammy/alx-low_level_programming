26ea1edbd473878ee6822a1a86f0df8dbaa7c905
Codammy <damisco005@gmail.com>
2023-01-23 05:18:02 -0800

    Variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,4 +1,5 @@
 #include <stdarg.h>
+#include <stdio.h>
 #include <string.h>
 
 /**
@@ -7,20 +8,48 @@
  * format: List of types of arguments passed to the function.
  * Return: Nothing.
  */
-void print_all(const char * const format, ...)
+void print_all(const char *const format, ...)
 {
-	int i, len;
+	int i = 0, len;
+	char c;
+	int in;
+	float f;
+	char *s;
+
 	va_list p;
 	len = strlen(format);
 
 	va_start(p, format);
 
-	while (i < format)
-	if (format[i] != "s" && format[i] != "c" && format[i] != "i" && format[i] != "f")
-		continue;
-	if (format[i] == "f")
-		printf(va_arg(p, double);
-	else
-		va_arg(p, int);
-	printf();
+	while (i < len)
+	{
+		if (format[i] != 's' && format[i] != 'c' && format[i] != 'i' && format[i] != 'f')
+		{i++;
+			continue;
+		}
+		if (format[i] == 'f')
+		{
+			f = va_arg(p, double);
+			printf("%f", f);
+		}
+		else if (format[i] == 's')
+		{
+			s = va_arg(p, char *);
+			printf("%s", s);
+		}
+		else if (format[i] == 'i')
+		{
+			in = va_arg(p, int);
+			printf("%d", in);
+		}
+		else if (format[i] == 'c')
+		{
+			c = va_arg(p, int);
+			printf("%c", c);
+		}
+		if (i < len - 1)
+			printf(", ");
+		i++;
+	}
+	putchar('\n');
 }
