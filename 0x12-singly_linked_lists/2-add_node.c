87b56359ae8315631c9db4db896f8643e5598bbb
Codammy <damisco005@gmail.com>
2023-01-27 01:10:54 -0800

    Linked list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,7 +13,7 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = (list_t *)malloc(sizeof(list_t));
 
-	if (new_node == NULL )
+	if (new_node == NULL)
 		return (NULL);
 	new_node->str = strdup(str);
 	new_node->len = strlen(str);
