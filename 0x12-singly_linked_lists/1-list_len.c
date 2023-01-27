e929763fde70aaefc33c925d62a0816260e01e31
Codammy <damisco005@gmail.com>
2023-01-26 20:58:21 -0800

    Linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stddef.h>
+
+/**
+ * list_len - function that returns the number of nodes
+ *
+ * @h: head node(starrting point).
+ * Return: number of nodes.
+ */
+size_t list_len(const list_t *h)
+{
+	unsigned int n = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
