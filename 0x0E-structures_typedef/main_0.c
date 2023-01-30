21c3ec313d9658026e700aea88ed8c488021af72
Codammy <damisco005@gmail.com>
2023-01-30 05:27:18 -0800

    structure and typedef

---
+++ 0x0E-structures_typedef/main_0.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * main - check the code .
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	dog_t *my_dog;
+
+	my_dog = new_dog(NULL);
+	if (my_dog == NULL)
+	{
+		printf("Failed\n");
+		return (1);
+	}
+	printf("My name is %s, I am %.2f, and my owner is %s\n",
+		my_dog->name, my_dog->age, my_dog->owner);
+	return (0);
+}
