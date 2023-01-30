d09616ccfbd65ab4b59daa464f923abe3d637d1e
Codammy <damisco005@gmail.com>
2023-01-30 04:12:15 -0800

    structure and typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,7 +15,7 @@ void print_dog(struct dog *d)
 		printf("Name: %s\n", d->name);
 
 	if (d->age == 0)
-		printf("Age: %d\n", 0);
+		printf("Age: %f\n", 0.0);
 	else
 		printf("Age: %f\n", d->age);
 
