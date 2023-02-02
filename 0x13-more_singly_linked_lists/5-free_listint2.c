c5760271778d39bb5d45c5cbbe4484489ee71feb
Codammy <damisco005@gmail.com>
2023-02-02 02:38:19 -0800

    Singly linked list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,7 +11,10 @@ void free_listint2(listint_t **head)
 	listint_t *tmp = *head;
 
 	if (head == NULL)
+	{
+		*head = NULL;
 		return;
+	}
 	while ((*head)->next != NULL)
 	{
 		tmp = (*head)->next;
