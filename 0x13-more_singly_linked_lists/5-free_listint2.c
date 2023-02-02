0c75d721400775b27aee5d4f34afed3947406ba6
Codammy <damisco005@gmail.com>
2023-02-02 02:05:16 -0800

    Singly linked list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free a listint_t list.
+ * @head: head node.
+ *
+ * Return: nothing.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp = *head;
+
+	if (*head == NULL)
+		return;
+	while ((*head)->next != NULL)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+	free(*head);
+	head = NULL;
+}
