848bceb060ce4ca4f7abeff767521d097f4450a2
Codammy <damisco005@gmail.com>
2023-02-09 07:28:10 -0800

    File handling

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+
+/**
+ * read_textfile -  reads a text file and prints it to the POSIX I/O.i
+ *
+ * @filename: file to read from.
+ * @letters: length of charcters to read.
+ * Return: 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	unsigned int lenrd;
+	char *lyrics;
+
+	if (filename == NULL)
+		exit(0);
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		exit(0);
+	lyrics = (char *)malloc(letters * sizeof(char));
+	if (lyrics == NULL)
+		exit(0);
+	lenrd = read(fd, lyrics, letters);
+
+	printf("%s\n", lyrics);
+	close(fd);
+	return (lenrd);
+}
