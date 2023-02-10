440cbcc7c1e241d383ac00fb80b398bc796c18ca
Codammy <damisco005@gmail.com>
2023-02-10 02:29:44 -0800

    File handling

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,21 +10,19 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	unsigned int lenrd, len;
+	unsigned int lenrd;
 	char *lyrics;
 
 	if (filename == NULL)
-		exit(0);
+		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
-		exit(0);
+		return (0);
 	lyrics = (char *)malloc(letters * sizeof(char));
 	if (lyrics == NULL)
-		exit(0);
+		return (0);
 	lenrd = read(fd, lyrics, letters);
-	len = dprintf(STDOUT_FILENO, lyrics, letters);
-		if (!len)
-			return (0);
+	 dprintf(STDOUT_FILENO, lyrics, letters);
 	close(fd);
 	return (lenrd);
 }
