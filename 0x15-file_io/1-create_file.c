6c3319d9f37bfa6c15664e756525dc90164d1e03
Codammy <damisco005@gmail.com>
2023-02-10 13:25:21 -0800

    File handling

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, wStats;
 
-	fd = open(filename, O_CREAT | O_EXCL | O_TRUNC | O_RDWR);
+	fd = open(filename, O_CREAT | O_TRUNC | 0600);
 	if (fd < 0)
 		return (-1);
 	if (text_content != NULL)
