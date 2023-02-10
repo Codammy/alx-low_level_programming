5ac36a8b410ee5c226527526109c85dc746008fe
Codammy <damisco005@gmail.com>
2023-02-10 13:20:12 -0800

    File handling

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, wStats;
 
-	fd = open(filename, O_TRUNC | O_RDWR | O_CREAT);
+	fd = open(filename, O_CREAT | O_EXCL | O_TRUNC | S_IRUSR | S_IWUSR);
 	if (fd < 0)
 		return (-1);
 	if (text_content != NULL)
---
+++ 0x15-file_io/cp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - program that copies the content of a file to another file.
+ *
+ * @argc - indicates no. of argument (MAX 3)
+ * @argv - arrays of pointers to argument. incluing program name.
+ */
+int main (int argc; char *argv[])
+{
+	int fd, size1, size2;
+	char *buf1, buf2;
+
+	fd = open(argv[1], O_RDONLY);
+	size1 = read
+	fd = argv[2];
+	return (0);
+}
