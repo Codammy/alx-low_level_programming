dd03346747c35558bdf519c76e06920612d537b0
Codammy <damisco005@gmail.com>
2023-02-11 01:40:08 -0800

    File handling

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,7 +9,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int fd;
+	int fd, rstats, wstats;
 	char *buff;
 
 	if (argc != 3)
@@ -18,26 +18,25 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 	fd = open(argv[1], O_RDONLY);
-	if (fd < 0)
+	buff = (char *) malloc(1024 * sizeof(char));
+	rstats = read(fd, buff, 1024);
+	if (rstats < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	buff = (char *) malloc(1024 * sizeof(char));
-	read(fd, buff, 1024);
 	if (close(fd) < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 	fd = open(argv[2], O_TRUNC | O_CREAT | O_RDWR, 0664);
-	if (fd < 0)
+	wstats = dprintf(fd, "%s", buff);
+	if (wstats < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	dprintf(fd, "%s", buff);
-
 	if (close(fd) < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
