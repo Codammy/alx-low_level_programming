9ff0428f30ea287d65177db8ad6898cecb17f00e
Codammy <damisco005@gmail.com>
2023-02-11 01:27:54 -0800

    File handling

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,7 +9,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int fd, rdstatus, clstatus, wrstatus;
+	int fd;
 	char *buff;
 
 	if (argc != 3)
@@ -18,28 +18,27 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 	fd = open(argv[1], O_RDONLY);
-	buff = (char *) malloc(1024 * sizeof(char));
-	rdstatus = read(fd, buff, 1024);
-	if (rdstatus < 0)
+	if (fd < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	clstatus = close(fd);
-	if (clstatus < 0)
+        buff = (char *) malloc(1024 * sizeof(char));
+        read(fd, buff, 1024);
+	if (close(fd) < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 	fd = open(argv[2], O_TRUNC | O_CREAT | O_RDWR, 0664);
-	wrstatus = dprintf(fd, "%s", buff);
-	if (wrstatus < 0)
+	if (fd < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	clstatus = close(fd);
-	if (clstatus < 0)
+	dprintf(fd, "%s", buff);
+
+	if (close(fd) < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
