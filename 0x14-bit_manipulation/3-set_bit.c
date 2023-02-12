b52b35bcd41b330ada45e2f954f301958967c320
Codammy <damisco005@gmail.com>
2023-02-12 13:16:37 -0800

    Bit manipulation

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * set_bit - sets the value of a bit at a given index.
+ * @n: decimal
+ * @index: index to srt.
+ *
+ * Return: 1 or -1..
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (n == NULL)
+		return (-1);
+	*n = 1 << index | *n;
+
+	return (1);
+}
