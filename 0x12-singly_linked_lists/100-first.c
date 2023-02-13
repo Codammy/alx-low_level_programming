29f4f3f10b060c6925931d1cc76f2c794f8c8bae
Codammy <damisco005@gmail.com>
2023-02-13 00:18:46 -0800

    Linked list

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * first - prints to stdo
+ *
+ * Return: 0.
+ */
+int main()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+
+	return (0);
+}
