0da6dcb942fd5b4adffbf98054a0fe7d80ef9f9a
Codammy <damisco005@gmail.com>
2023-02-15 00:26:33 -0800

    malloc calloc realloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * malloc_checked - Allocates memory using malloc.
+ *
+ * @b: malloc's param
+ * Return: pointer to newly allocated memory.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *maloc =  malloc(b * sizeof(b));
+	if (maloc == NULL)
+		exit(98);
+
+	return (maloc);
+}
