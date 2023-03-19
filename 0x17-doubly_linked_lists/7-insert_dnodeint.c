2d5deabfe2d4ec5834443807d9812d6d7400b99b
Codammy <damisco005@gmail.com>
2023-03-19 03:25:07 -0700

    C -Doubly linked lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,7 +21,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	newNode->n = n;
 	newNode->next = NULL;
 	newNode->prev = NULL;
-	if (tmp == NULL && n == 0)
+	if (tmp == NULL && idx == 0)
 	{
 		*h = newNode;
 		return (*h);
