75e92d8d6d20c8ed60ec2e899fe245982ef1b322
Codam <damisco005@gmail.com>
2023-06-16 23:03:42 +0000

    modified a

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,15 +10,19 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned long int i;
 	hash_table_t *table;
 
 	table = malloc(sizeof(hash_table_t));
 	if (!table)
 		return (NULL);
-	table->array = malloc(sizeof(hash_node_t) * size);
+	table->array = malloc(size * sizeof(hash_node_t*));
 	if (!table->array)
 		return (NULL);
 	table->size = size;
+
+	for(i = 0; i < size; i++)
+		table->array[i] = NULL;
 
 	return (table);
 }
