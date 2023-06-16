b42e038de3ed5c2786efe140c168f7bd7b4eb981
Codam <damisco005@gmail.com>
2023-06-16 23:14:31 +0000

    modified hash_tables.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,3 +1,5 @@
+#ifndef _HEADER_
+#define _HEADER_
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -8,9 +10,9 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -23,8 +25,10 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+#endif
