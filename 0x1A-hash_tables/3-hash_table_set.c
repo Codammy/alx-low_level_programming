a9b9711b89cd9c7eb8883536975722d8d288deec
Codam <damisco005@gmail.com>
2023-06-18 14:56:53 +0100

    modified e

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,11 +11,11 @@
  * @value: value
  * Return: 1 if suceeded otherwise 0
  */
-void handle_collision(hash_node_t *item, hash_node_t *head, hash_node_t *cur_item, const char* key, const char *value);
+void handle_collision(hash_node_t *cur, hash_node_t *item, hash_node_t *head);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *item, *cur_item, *head;
-	int hash_value;
+	hash_node_t *item, *head;
+	int hash;
 
 	if (!(item = malloc(sizeof(hash_node_t))))
 		return (0);
@@ -30,29 +30,29 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (item->next == NULL)
 		head = item->next;
 
-	hash_value = key_index((unsigned char *)key, ht->size);
-	if (ht->array[hash_value])
-		handle_collision(item, key, value);
+	hash = key_index((unsigned char *)key, ht->size);
+	if (ht->array[hash])
+		handle_collision(ht->array[hash], item, head);
 	else
 	{
-		ht->array[hash_value] = item;
+		ht->array[hash] = item;
 	}
 	return (1);
 }
-void handle_collision(hash_node_t *item, hash_node_t *head, hash_node_t *cur_item, const char *key, const char *value)
+void handle_collision(hash_node_t *cur, hash_node_t *item, hash_node_t *head)
 {
-if (strcmp(cur_item->key, item->key) == 0)
-	strcpy(item->key, cur_item->value);
+if (strcmp(cur->key, item->key) == 0)
+	strcpy(item->key, cur->value);
 else
 {
-/* handle collisions */
-if (head == NULL)
-head->next = item->next;
-else
-{
-item->next->next = head;
-head = item->next;
-}
+	/* handle collisions */
+	if (head == NULL)
+		head = item;
+	else
+	{
+		item->next = head;
+		head = item;
+	}
 }
 
 }
