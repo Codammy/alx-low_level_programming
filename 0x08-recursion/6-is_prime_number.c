6e833c211b961b3d9733a7096f417b7366882837
CODAM <damisco005@gmail.com>
2023-07-05 16:12:38 +0100

    modified a.out 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,24 +1,9 @@
-
-/**
- * is_prime_number - tell if a number us primw or not.
- *
- * @n: argument
- * Return: 1 or 0.
- */
-int isPrime(int n, int dvs);
-int is_prime_number(int n)
-{
-	if (n <= 1)
-		return (0);
-	return (isPrime(n, n - 1));
-}
 /**
  * isPrime - work is_prime_number
  *
  * @n: argument1
  * @dvs: argument2
- * Return: 1 or 0
- */
+ * Return: 1 or 0                                                               */
 int isPrime(int n, int dvs)
 {
 	if (dvs == 1 && n != 2)
@@ -26,4 +11,16 @@ int isPrime(int n, int dvs)
 	if (n % dvs == 0)
 		return (0);
 	return (isPrime(n, dvs - 1));
+}
+/**
+ * is_prime_number - tell if a number us primw or not.
+ *
+ * @n: argument
+ * Return: 1 or 0.
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (isPrime(n, n - 1));
 }
