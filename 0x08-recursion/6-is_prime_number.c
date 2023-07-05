a12cce04e865b261105a49a1e625757d5259843c
CODAM <damisco005@gmail.com>
2023-07-05 15:34:34 +0100

    Hands on recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,17 +1,26 @@
 
 /**
  * is_prime_number - tell if a number us primw or not.
+ *
  * @n: argument
  * Return: 1 or 0.
  */
+int isPrime(int n, int dvs);
 int is_prime_number(int n)
 {
-	if (n == 1)
-		return (0);
-	if (n == 2)
+	return (isPrime(n, n / 2));
+}
+/**
+ * isPrime - work is_prime_number
+ *
+ * @n: argument1
+ * @dvs: argument 2
+ * Return 1 or 0
+int isPrime(int n, int dvs)
+{
+	if (dvs == 1)
 		return (1);
-	else if ((n % 2 == 1) && (n % 3 != 0) && (n % 5 != 0) && (n % 7 != 0))
-		return (1);
-	else
+	if (n % dvs == 0)
 		return (0);
+	return (isPrime(n, dvs - 1));
 }
