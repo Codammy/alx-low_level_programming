1753d9ce5e1870e893e8e03852acc8505805b7da
Codam <damisco005@gmail.com>
2023-07-10 22:22:42 +0000

    modified 2-str_concat.c 1-strdup.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,7 +12,7 @@ char *str_concat(char *s1, char *s2)
 	char *compsent;
 	int len1, len2, len, i, j;
 
-	if (!s1 || !s2)
+	if (!s1 && !s2)
 		return (NULL);
 	len1 = strlen(s1);
 	len2 = strlen(s2) + 1;
