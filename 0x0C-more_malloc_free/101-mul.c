6dc73b154ca05d4892d491f3688da0f2e4b5ca12
Codam <damisco005@gmail.com>
2023-07-16 06:54:54 +0000

    recently modified 101-mul.c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - simple calculator
+ *
+ * @argc: numarg
+ * @argv: ptr to arr of ptr
+ * Return: 0 on Success.
+ */
+int main(int argc, char **argv)
+{
+	int num1, num2;
+	
+	if (argc != 3)
+	{
+		printf("Error");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	printf("%d\n", num1 * num2);
+
+	return (0);
+}
