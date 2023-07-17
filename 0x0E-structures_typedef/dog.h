7a6a1fa0270be14b49f5ddac89900958fca70dfb
Codam <damisco005@gmail.com>
2023-07-17 11:52:36 +0000

    recently modified dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,16 @@
 #ifndef _DOG_
 #define _DOG_
 /**
- * dog - dog structure
- * 
+ * struct dog - dog structure
+ *
  * @name: name of the dog
  * @age: How old is it.
  * @owner: Dog's owner
  */
-struct dog {
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
-}
+};
 #endif
