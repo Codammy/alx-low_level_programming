28405985f3aab8f5aa4ffe8739173a3d85f698e0
Codam <damisco005@gmail.com>
2023-07-17 12:37:49 +0000

    recently modified 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,21 +9,21 @@
 void print_dog(struct dog *d)
 {
 	if (!d)
-		printf("nil\n");
+		printf("(nil)\n");
 	if (d)
 	{
 		if (!d->name)
-			printf("nil\n");
+			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", d->name);
 
 		if (!d->age)
-			printf("nil\n");
+			printf("Age: (nil)\n");
 		else
 			printf("Age: %f\n", d->age);
 
 		if (!d->owner)
-			printf("nil\n");
+			printf("Owner: (nil)\n");
 		else
 			printf("Owner: %s\n", d->owner);
 	}
