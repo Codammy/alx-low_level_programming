b1df999608f1b6b99ae12bf7f1f261b4b090acf4
Codam <damisco005@gmail.com>
2023-07-21 22:41:53 +0100

    modified 2-int_index.c 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,6 +12,8 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
+	if (!array || !action || (size <= 0))
+		return;
 	for (i = 0; i < size; i++)
 		action(array[i]);
 }
