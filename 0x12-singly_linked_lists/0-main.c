5aa3b034562cf8c17d9fa4804871fe54889b6ddc
Codam <damisco005@gmail.com>
2023-07-28 02:37:33 +0100

    modified a 0-print_list.c

--- 0x12-singly_linked_lists/0-main.c
+++ 0x12-singly_linked_lists/0-main.c
@@ -27,13 +27,13 @@ int main(void)
     new->next = head;
     head = new;
     n = print_list(head);
-    printf("-> %lu elements\n", n);
+    printf("-> %u elements\n", n);
 
     printf("\n");
     free(new->str);
     new->str = NULL;
     n = print_list(head);
-    printf("-> %lu elements\n", n);
+    printf("-> %u elements\n", n);
 
     free(new);
     return (0);
--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,4 +6,17 @@
  */
 size_t print_list(const list_t *h)
 {
+	size_t i = 0;
+
+	while(h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		i++;
+		h = h->next;
+	}
+
+	return (i);
 }
