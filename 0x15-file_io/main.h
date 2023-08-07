9d036af5371a2cae877da21a265a00b65ab75eb8
Codam <damisco005@gmail.com>
2023-08-07 21:02:13 +0000

    0-read_textfile.c

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN
+#define MAIN
+int create_file(const char *filename, char *text_content);
+ssize_t read_textfile(const char *filename, size_t letters);
+int append_text_to_file(const char *filename, char *text_content);
+#endif
