50a610633cd183b743b6a4e88b73f4cb27628426
Codam <damisco005@gmail.com>
2023-08-08 05:30:35 +0100

    modified a

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,7 +4,11 @@
 #include "main.h"
 
 /**
+ * read_textfile - reads a text file and prints it to the standard output.
  *
+ * @filename: file toread from
+ * @letters: number of bytes to read
+ * Return: bytes read
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -13,14 +17,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (!filename || letters < 1)
 		return (0);
-	if ((fd = open(filename, O_RDONLY)) < 1)
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
 		return (0);
-	if (!(buff = malloc(sizeof(char) * letters)))
+	buff = malloc(letters);
+	if (!buff)
 		return (0);
-	if ((bytes_read = read(fd, buff, letters)) < 0)
+	bytes_read = read(fd, buff, letters);
+	if (bytes_read < 0)
 		return (0);
-	if ((bytes_read = write(STDIN_FILENO, buff, letters)) < 0)
-                return (0);
+	bytes_read = write(STDOUT_FILENO, buff, letters);
+	if (bytes_read < 0)
+		return (0);
 
 	return (bytes_read);
 }
