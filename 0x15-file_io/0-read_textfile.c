a34267eeba5a0a1a4524aa6fae19c2583ff39e47
Codam <damisco005@gmail.com>
2023-08-08 05:02:22 +0100

    modified 0-main.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,2 +1,26 @@
 #include <unistd.h>
-#include "main.h" 
+#include <stdlib.h>
+#include <fcntl.h>
+#include "main.h"
+
+/**
+ *
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int bytes_read, fd;
+	char *buff;
+
+	if (!filename || letters < 1)
+		return (0);
+	if ((fd = open(filename, O_RDONLY)) < 1)
+		return (0);
+	if (!(buff = malloc(sizeof(char) * letters)))
+		return (0);
+	if ((bytes_read = read(fd, buff, letters)) < 0)
+		return (0);
+	if ((bytes_read = write(STDIN_FILENO, buff, letters)) < 0)
+                return (0);
+
+	return (bytes_read);
+}
