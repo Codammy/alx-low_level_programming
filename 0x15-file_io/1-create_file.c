b7620a995a7121a97bde01a6670957d1f9d61cde
Codam <damisco005@gmail.com>
2023-08-08 06:08:28 +0000

    b

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <string.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -18,15 +19,20 @@ int create_file(const char *filename, char *text_content)
 	if (!filename || !text_content)
 		return (-1);
 	fd = open(filename, O_WRONLY);
-	if (fd < 0)
-		return (-1);
-	else
-		fd = open(filename, O_WRONLY, O_CREAT);
-	if (fd < 0)
-		return (-1);
+	if (fd != 3)
+	{
+		fd = creat(filename, 600);
+		if (fd != 3)
+		{
+			printf("Error %d", fd);
+			return (-1);
+		}
+		fd = open(filename, O_WRONLY | O_CREAT);
+		if (fd < 0)
+			return (-1);
+	}
 	n = write(fd, text_content, strlen(text_content));
-	/**
 	if (n < 0)
-		return (-1);*/
+		return (-1);
 	return (1);
 }
--- 0x15-file_io/main/main.h
+++
@@ -1,6 +0,0 @@
-#ifndef MAIN
-#define MAIN
-int create_file(const char *filename, char *text_content);
-ssize_t read_textfile(const char *filename, size_t letters);
-int append_text_to_file(const char *filename, char *text_content);
-#endif
