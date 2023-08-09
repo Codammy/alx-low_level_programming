c6d0099c79fb9000165908d5b27e3cdee85aeafe
Codam <damisco005@gmail.com>
2023-08-09 11:21:20 +0100

    modified hello

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_WRONLY);
+	fd = open(filename, O_WRONLY | O_TRUNC);
 	if (fd < 3)
 	{
 		fd = open(filename, O_WRONLY | O_CREAT, 0600);
