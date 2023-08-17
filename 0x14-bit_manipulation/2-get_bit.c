3b21971c7b1334f16dc86c1e3c3280ec9de486ed
Codam <damisco005@gmail.com>
2023-08-17 07:44:24 +0100

    modified c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -17,8 +17,10 @@ int get_bit(unsigned long int n, unsigned int index)
 		ref >>= 1;
 		len++;
 	}
-	if (index > len)
-		return (-1);
+	/*
+	* if (index > len)
+	*	return (-1);
+	*/
 	num = 1 << (index);
 	while (i <= len)
 	{
