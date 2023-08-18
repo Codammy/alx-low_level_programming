9249dd16a5e23b36acff42bfd07fd9a89a606188
Codam <damisco005@gmail.com>
2023-08-18 07:13:52 +0100

    modified 5-flip_bits.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,9 +8,9 @@
  * @index: index to set
  * Return: 1 on Success
  */
-int clear_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int ref = *n, len = -1, i = 0, num;
+	unsigned long int ref = *n, len = -1, num;
 
 	while (ref > 0)
 	{
@@ -19,16 +19,7 @@ int clear_bit(unsigned long int *n, unsigned int index);
 	}
 	if (index > len)
 		return (-1);
-	num = 1;
-	while (i <= len)
-	{
-		if (i == index)
-		{
-			*n = *n | num;
-			return (1);
-		}
-		num <<= 1;
-		i++;
-	}
+	num = ~(1 << index);
+	*n = *n & num;
 	return (1);
 }
--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,5 +9,5 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-
+	unsigned int ref = 
 }
