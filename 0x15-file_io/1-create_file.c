7235f724b3eeb73cee1618d1d85c018d4f86350f
Codam <damisco005@gmail.com>
2023-09-05 13:53:33 +0100

    modified 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (!text_content)
 	{
-		close(fd)
+		close(fd);
 		return (1);
 	}
 	n = write(fd, text_content, strlen(text_content));
