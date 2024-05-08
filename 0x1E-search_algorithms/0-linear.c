223738a05c02bbce64b4d9a2f29e98a04c03de1a
Dami <damisco005@gmail.com>
2024-05-08 09:18:50 +0100

    a.out 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,7 +14,7 @@ int linear_search(int *array, size_t size, int value)
 
 	if (array)
 	{
-		while (i < size)
+		while (i < (int)size)
 		{
 			printf("Value checked array[%d] = [%d]\n", i, *array);
 			if ((*array) == value)
