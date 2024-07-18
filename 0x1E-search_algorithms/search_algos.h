99523b0f6f0a96affb1895b7dc2d04c2e30e2743
Dami <damisco005@gmail.com>
2024-07-18 07:50:40 +0100

    search_algos.h 0-linear.c

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -12,9 +12,9 @@
  */
 typedef struct listint_s
 {
-    int n;
-    size_t index;
-    struct listint_s *next;
+	int n;
+	size_t index;
+	struct listint_s *next;
 } listint_t;
 
 /**
@@ -29,10 +29,10 @@ typedef struct listint_s
  */
 typedef struct skiplist_s
 {
-    int n;
-    size_t index;
-    struct skiplist_s *next;
-    struct skiplist_s *express;
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
 } skiplist_t;
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
